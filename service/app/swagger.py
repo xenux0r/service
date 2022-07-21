f6b759ef02bb1e7756cd15d99debc53276677e28
xenu-xore <xenu-xore>
2022-07-21 09:16:40 +0300

    Init commit

--- service/app/swagger.py
+++ service/app/swagger.py
@@ -6,27 +6,3 @@ api = Api(version='1.0', title='Функциональный сервис с и�
           description='Сервис для работы с API VK', doc='/docs/',
           authorizations=authorizations
           )
-
-
-group = api.model('GroupSchema', {
-        'id': fields.Integer,
-        'name': fields.String
-})
-
-create_group = api.model('CreateGroupSchema', {
-        'name': fields.String
-})
-
-user = api.model('UserSchema', {
-        'username': fields.String,
-        'password': fields.String
-})
-user_group = api.model('UserGroupsSchema', {
-        'username': fields.String,
-        'groups': fields.Nested(group, as_list=True)
-})
-
-user_subscribe = api.model('UserFollowerSchema', {
-        'id': fields.Integer,
-        'username': fields.String
-})
